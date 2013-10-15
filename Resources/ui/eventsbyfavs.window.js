97bf62546f612005ed3f19aa5d008b384b412e64
AppWerft <rs@hamburger-appwerft.de>
2013-10-15 13:09:51 +0200

    screenshots

--- Resources/ui/eventsbyfavs.window.js
+++ Resources/ui/eventsbyfavs.window.js
@@ -6,15 +6,59 @@ exports.create = function() {
 		backgroundColor : '#00597C',
 		title : 'Meine Nachttour'
 	});
-
+	var eventtemplate = require('ui/TEMPLATES').eventrow;
+	self.listView = Ti.UI.createListView({
+		backgroundColor : '#00597C',
+		templates : {
+			'event' : eventtemplate
+		},
+		defaultItemTemplate : 'event',
+	});
+	var sections = [];
 	var updateList = function() {
-		var favs = Ti.App.NdW.getFavs();
-		if (favs.length == 0) {
+		var events = Ti.App.NdW.getFavs();
+		if (events.length == 0) {
 			alert('Hier werden die von Dir vorgemerkten Ereignisse angezeigt. Du kannst Sie mit dem Stern auf dr Ereignisseite vormerken.')
-		}
-		return;
-	};
 
-	self.addEventListener('focus', updateList)
+			return;
+		}
+		var data = [];
+		for (var i = 0; i < events.length; i++) {
+			var event = events[i];
+			data.push({
+				kind : {
+					image : (event['kinderprogramm']) ? '/assets/forkind.png' : '/assets/null.png'
+				},
+				rollstuhl : {
+					image : (event['nicht_barrierefrei']) ? '/assets/norollstuhl.png' : '/assets/rollstuhl.png'
+				},
+				title : {
+					text : event.titel
+				},
+				zeit : {
+					text : (event.zeit) ? event.zeit + '  ' : null
+				},
+				properties : {
+					itemId : JSON.stringify(event),
+					accessoryType : Ti.UI.LIST_ACCESSORY_TYPE_DISCLOSURE
+				}
+			});
+		}
+		sections[0] = Ti.UI.createListSection({
+			items : data
+		});
+		self.listView.setSections(sections);
+	};
+	self.add(self.listView);
+	self.addEventListener('focus', updateList);
+	self.listView.addEventListener('itemclick', function(_e) {
+		var win = require('ui/event.window').create(_e.itemId);
+		if (Ti.Android)
+			win.open();
+		else
+			self.tab.open(win, {
+				animate : true
+			});
+	});
 	return self;
 };
